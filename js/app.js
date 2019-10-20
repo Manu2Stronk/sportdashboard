a53502863c8efe92f0eab0ee3b1b2c44ae119cfc
Manu2Stronk <manuel.koehrer@web.de>
2019-10-20 17:58:35 +0200

    Initial structure and basic app.js

---
+++ js/app.js
@@ -0,0 +1,28 @@
+class App{
+  constructor (pageList){
+    this._pages = pageList;
+  }
+
+  run(){
+    window.addEventListener("hashchange", ()=> {
+        console.log("hashchange");
+        this._handleRoute();
+    });
+  }
+
+  _handleRoute(){
+    let pageUrl = location.hash.slice(1);
+    let matches = null;
+
+    let page = this._pages.find(p => matches = pageUrl.match(p.url));
+    location.href = ("." + page.resource);
+
+    this.currentPageObj = new page.klasse(this);
+    this.currentPageObj.show();
+  }
+  
+  setPageContent(htmlContent){
+    let container = document.querySelector("#content");
+    container.innerHTML = htmlContent;
+  }
+}
