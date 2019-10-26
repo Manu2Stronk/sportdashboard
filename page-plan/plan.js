5d6e6f6f16c94ba41a4c44c7858711ef2ecf5370
TillSchirrm <till.schirrmei@gmail.com>
2019-10-27 01:18:28 +0200

    creating workout class

--- page-plan/plan.js
+++ page-plan/plan.js
@@ -7,7 +7,10 @@ class Plan {
     console.log(divWorkout);
     divWorkout.style.display = "block";
   }
-  save(divWorkout){
+  save(divWorkout, title){
+
+    console.log("title: " + title);
+    // let workout = new Workout();
     console.log("save()");
     divWorkout.style.display = "none";
     // def div-Element
@@ -18,9 +21,11 @@ class Plan {
     buttonElement.className += "buttonCloseWorkout";
     buttonElement.value += "x";
     buttonElement.type  = "button";
-
+    buttonElement.id = "buttonCloseWorkout";
     // add to dropdown
     dropdown.appendChild(divElement);
     divElement.appendChild(buttonElement);
+
+    return buttonElement;
   }
 }
---
+++ page-plan/workout.js
@@ -0,0 +1,11 @@
+class Workout {
+  constructor(id, title, date, distance, duration, kindOfSport, discription) {
+    this.id = id;
+    this.title = title;
+    this.date = date;
+    this.distance = distance;
+    this.duration = duration;
+    this.kindOfSport = kindOfSport;
+    this.discription = discription;
+  }
+}
