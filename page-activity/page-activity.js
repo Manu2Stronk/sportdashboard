84b01893df77b29448c6143649d8188529410226
Manu2Stronk <manuel.koehrer@web.de>
2019-10-27 18:12:36 +0100

    Added basic activity table + load from firestore logic

--- page-activity/page-activity.js
+++ page-activity/page-activity.js
@@ -9,5 +9,71 @@ class pageActivity{
       htmlContent = await html.text();
     }
     this._app.setPageContent(htmlContent);
+
+    this.loadAllActivities();
   }
+
+    async loadAllActivities() {
+      //clear the table
+      var oActivityTable = document.getElementById("idActivityTableBody");
+      while (oActivityTable.firstChild) {
+        oActivityTable.removeChild(oActivityTable.firstChild);
+      }
+
+      //load activities from the firebase firestore
+      var oQuerySnapshot = await firebase.firestore().collection("activities").get();
+      oQuerySnapshot.docs.forEach((oDocument) => {
+        var oActivity = oDocument.data();
+        console.log(oActivity);
+        this.renderActivity(oActivity);
+      })
+    }
+
+    renderActivity(oActivity) {
+      var oActivityTable = document.getElementById("idActivityTableBody"),
+          temp = document.createElement('tr'),
+          sRow = "";
+
+      sRow += "<td>" + this.formatDate(oActivity) + "<br/>" + oActivity.date.getFullYear() + "</td>";
+      sRow += "<td>" + oActivity.name + "<br/>" + oActivity.type + "</td>";
+      sRow += "<td>" + oActivity.distance + " km" + "</td>";
+      sRow += "<td>" + this.formatDuration(oActivity) + "</td>";
+      sRow += "<td>" + this.formatAverageSpeed(oActivity) + "</td>";
+      sRow += "<td>" + oActivity.cal + " kcal" + "</td>";
+
+      temp.innerHTML = sRow;
+      oActivityTable.appendChild(temp);
+    }
+
+    formatDate(oActivity) {
+        var monthNames = [
+          "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+        ];
+
+        var day = oActivity.date.getDate(),
+            monthIndex = oActivity.date.getMonth();
+        return monthNames[monthIndex] + " "+ day;
+    }
+
+
+    formatDuration(oActivity) {
+      return oActivity.dauerHH + ":" + oActivity.dauerMM + ":" + oActivity.dauerSS;
+    }
+
+    formatAverageSpeed(oActivity) {
+      return this.calculateAverageSpeed(oActivity) + " km/h";
+    }
+
+    calculateAverageSpeed(oActivity) {
+      var iDuraitonInSeconds = oActivity.dauerHH * 60 * 60 +
+          oActivity.dauerMM * 60 +
+          oActivity.dauerSS,
+          iAvgSpeed = oActivity.distance / iDuraitonInSeconds * 3600;
+
+      return Math.floor(iAvgSpeed * 100) / 100;
+    }
 }
+
+ActivityPage = new pageActivity();
+
+setTimeout(() => {ActivityPage.loadAllActivities()}, 2000);
