142e38237cc40c57fe2ed2ad28f76acd40c2c47b
LiamW98 <liam.wallraff@web.de>
2019-11-02 18:51:54 +0100

    Further work on sorting data by Dates

--- page-analytic/page-analytic.js
+++ page-analytic/page-analytic.js
@@ -9,58 +9,97 @@ class pageAnalytic{
       htmlContent = await html.text();
     }
     this._app.setPageContent(htmlContent);
-    this.loadAllActivities();
+    this.getAndLoadData();
   }
 
 
-
-async loadAllActivities() {
-  var sLabelCalorie = 1;
+// Receive Data from Firebase and insert it into Labels on Analytical Page
+async getAndLoadData() {
   var oLabelCalorie = document.getElementById('calorieID');
-  var today = new Date();
-  var date1 = today.getDate();
+  var oLabelHeart = document.getElementById('heartrateID');
+  var oLabelDuration = document.getElementById('durationID');
+  var oLabelDistance = document.getElementById('distanceID');
+  var oLabelwklDuration = document.getElementById('wklDurationID');
+  var oLabelwklDistance = document.getElementById('wklDistanceID');
+  var oLabelwklHeart = document.getElementById('wklHeartID');
+  var oLAbelwklCal = document.getElementById('wklCalID');
+  var sLabelCalorie = 0;
+  var sLabelHeart = 0;
+  var sLabelDurationHH = 0;
+  var sLabelDurationMM = 0;
+  var sLabelDurationSS = 0;
+  var sLabelwklDurationHH = 0;
+  var sLabelwklDurationMM = 0;
+  var sLabelwklDurationSS = 0;
+  var sLabelDistance = 0;
+  var sLabelwklDistance = 0;
+  var sLabelwklHeart = 0;
+  var sLabelwklCal = 0;
 
+  // oActivity.heart_rate;
+  var today = new Date();
+
+  var dateNow = today.getTime();
+
+  // //
+  // var date3 = date1 + date2;
+  // console.log(date3);
+
+  var i = 0;
+  var ActivityArray = [];
   //load activities from the firebase firestore
   var oQuerySnapshot = await firebase.firestore().collection("activities").get();
   oQuerySnapshot.docs.forEach((oDocument) => {
     var oActivity = oDocument.data();
-    var date2 = oActivity.date.getDate();
-    var date3 = date1 + date2;
-    this.getValues(oActivity, sLabelCalorie);
-    sLabelCalorie = sLabelCalorie + oActivity.cal;
-    console.log(date3);
+    var dateAct = oActivity.date.getTime();
+    var dateTemp = dateNow - dateAct
+    if (dateTemp <= 604800000) {
+      sLabelwklDistance += oActivity.distance;
+      sLabelwklDurationHH += oActivity.durationHH;
+      sLabelwklDurationMM += oActivity.durationMM;
+      sLabelwklDurationSS += oActivity.durationSS;
 
+    }
+
+
+
+    // this.getValues(oActivity, sLabelCalorie);
+    sLabelCalorie = sLabelCalorie + oActivity.cal;
   })
+
+  // for (var k = 0; k < ActivityArray.length; k++) {
+  //   console.log(ActivityArray[k]);
+  // }
+
+
   oLabelCalorie.innerHTML = sLabelCalorie;
 }
-getValues(oActivity, sLabelCalorie){
-  var oLabelHeart = document.getElementById('heartrateID'),
-  sLabelHeart = oActivity.heart_rate;
-  oLabelHeart.innerHTML = sLabelHeart;
-
-  var oLabelDuration = document.getElementById('durationID'),
-  sLabelDurationHH = oActivity.durationHH,
-  sLabelDurationMM = oActivity.durationMM,
-  sLabelDurationSS = oActivity.durationSS;
-  oLabelDuration.innerHTML = sLabelDurationHH + ":" + sLabelDurationMM + ":" + sLabelDurationSS;
-
-  // var oLabelCalorie = document.getElementById('calorieID');
-  sLabelCalorie = sLabelCalorie + oActivity.cal;
-  console.log(oActivity.cal, sLabelCalorie);
-  // oLabelCalorie.innerHTML = sLabelCalorie;
-
-  // var oLabelwklDuration = document.getElementById('wklDurationID'),
-  // sLabelwklDuration = oActivity.cal;
-  // oLabelwklDuration.innerHTML = sLabelwklDuration;
-
-  // var oLabelwklDistance = document.getElementById('wklDistanceID'),
-  // sLabelwklDistance = oActivity.cal;
-  // oLabelwklDistance.innerHTML = sLabel;
-
-  var oLabelDistance = document.getElementById('distanceID'),
-  sLabelDistance = oActivity.distance;
-  oLabelDistance.innerHTML = sLabelDistance;
-}
+// getValues(oActivity, sLabelCalorie){
+//
+//
+//   oLabelHeart.innerHTML = sLabelHeart;
+//
+//
+//
+//   oLabelDuration.innerHTML = sLabelDurationHH + ":" + sLabelDurationMM + ":" + sLabelDurationSS;
+//
+//   // var oLabelCalorie = document.getElementById('calorieID');
+//   sLabelCalorie = sLabelCalorie + oActivity.cal;
+//   console.log(oActivity.cal, sLabelCalorie);
+//   // oLabelCalorie.innerHTML = sLabelCalorie;
+//
+//   // ,
+//   //  oActivity.cal;
+//   // oLabelwklDuration.innerHTML = sLabelwklDuration;
+//
+//   // ,
+//   //  = oActivity.cal;
+//   // oLabelwklDistance.innerHTML = sLabel;
+//
+//   ,
+//   = oActivity.distance;
+//   oLabelDistance.innerHTML = sLabelDistance;
+// }
 
 }
 
