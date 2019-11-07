99b1442084bf031d1339edb7743f5b9c9073de7c
Manu2Stronk <manuel.koehrer@web.de>
2019-11-07 15:08:31 +0100

    Collection Workouts in Firebase created and added forEach

--- page-plan/page-plan.js
+++ page-plan/page-plan.js
@@ -15,20 +15,12 @@ class pagePlan {
   }
 
   async loadAllActivities() {
-    //
-    // var firebaseConfig = {
-    //   apiKey: "AIzaSyDQJNTCymv5mP0bAjkdbab-cWpAVZFJHMo",
-    //   authDomain: "trainingsplan-88a5b.firebaseapp.com",
-    //   databaseURL: "https://trainingsplan-88a5b.firebaseio.com",
-    //   projectId: "trainingsplan-88a5b",
-    //   storageBucket: "trainingsplan-88a5b.appspot.com",
-    //   messagingSenderId: "189292657427",
-    //   appId: "1:189292657427:web:71e70144eacecb3b7c78e6",
-    //   measurementId: "G-8ZSZVF71V1"
-    // };
-    // let firebaseTrainingsplan = new FirebaseTrainingsplan();
-    // let firebase = firebaseTrainingsplan.init();
-    // firebaseTrainingsplan.write(firebase);
+      //load activities from the firebase firestore
+      var oQuerySnapshotPlan = await firebase.firestore().collection("workouts").get();
+      oQuerySnapshot.docs.forEach((oDocument) => {
+        //Funktion die Pro Eintrag ausgeführt werden soll
+      })
+    }
 
     console.log("page-plan.html running");
     let buttonAddWorkout = document.getElementById("buttonAddWorkout");
